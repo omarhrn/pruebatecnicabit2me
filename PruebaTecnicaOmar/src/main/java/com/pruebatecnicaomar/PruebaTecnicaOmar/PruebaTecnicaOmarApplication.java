79ec2f032cf9a0ede4416b4fce841ea934d78cde
omarhrn <omarr1354@gmail.com>
2021-01-17 03:30:15 +0100

    Creacion proyecto Spring

---
+++ PruebaTecnicaOmar/src/main/java/com/pruebatecnicaomar/PruebaTecnicaOmar/PruebaTecnicaOmarApplication.java
@@ -0,0 +1,13 @@
+package com.pruebatecnicaomar.PruebaTecnicaOmar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaOmarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaOmarApplication.class, args);
+	}
+
+}
